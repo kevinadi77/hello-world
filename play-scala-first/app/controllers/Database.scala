6eaaadccaf1f51b67f5cdf394e9d5fde643bef64
Kevin Adistambha <kevinadi@gmail.com>
2015-09-25 17:38:21 +1000

    added database to play-scala

---
+++ play-scala-first/app/controllers/Database.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.db._
+import play.api.Play.current
+
+class Database extends Controller {
+
+  def index = Action {
+    var outstring = ""
+    val conn = DB.getConnection()
+    try {
+      val stmt = conn.createStatement
+      val rs = stmt.executeQuery("SELECT 1 AS COL")
+      while (rs.next()) {
+        outstring += rs.getString("COL")
+      }
+    } finally {
+      conn.close
+    }
+    Ok(outstring)
+  }
+
+  def indexautoclose = Action {
+    var outstring = ""
+    DB.withConnection { conn =>
+      val stmt = conn.createStatement
+      val rs = stmt.executeQuery("SELECT 'DB IS ALIVE' AS COL")
+      while (rs.next()) {
+        outstring += rs.getString("COL")
+      }
+    }
+    Ok(outstring)
+  }
+
+}
