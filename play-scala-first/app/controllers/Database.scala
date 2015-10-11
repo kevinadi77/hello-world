ea795b95602de0a7ec34fcfff0bd3773ce796a71
Kevin Adistambha <kevinadi@gmail.com>
2015-10-11 22:51:08 +1100

    using anorm & json

--- play-scala-first/app/controllers/Database.scala
+++ play-scala-first/app/controllers/Database.scala
@@ -5,8 +5,11 @@ import play.api.mvc._
 import play.api.db._
 import play.api.Play.current
 
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
 import anorm._
-// import anorm.SqlParser._
+// import anorm.SqlParser._ /* commented to make the parser .get method explicit */
 
 
 
@@ -14,26 +17,69 @@ case class Blah(key:String, value:String)
 
 object Blah {
 
-  val parse = {
+  /*
+  Parse SQL query output into structure Blah
+  */
+  private val parse = {
     SqlParser.get[String]("KEY") ~
     SqlParser.get[String]("VALUE") map {
       case key ~ value => Blah(key,value)
     }
   }
 
+  /*
+  Get all Blah from the database, returns Blah array
+  */
   def findAll(): Seq[Blah] = {
     DB.withConnection { implicit conn =>
-      SQL("SELECT * FROM TEST").as(Blah.parse *)
+      SQL(
+        """
+        SELECT * FROM TEST
+        """
+      ).as(Blah.parse *)
     }
   }
 
+  /*
+  Insert a new Blah into the database
+  */
   def create(blah: Blah): Unit = {
     DB.withConnection { implicit conn =>
-      SQL("INSERT INTO TEST (KEY,VALUE) VALUES ({key},{value})")
-        .on('key -> blah.key, 'value -> blah.value)
-        .executeUpdate()
+      SQL(
+        """
+        INSERT INTO TEST (KEY,VALUE) VALUES ({key},{value})
+        """
+      ).on('key -> blah.key, 'value -> blah.value)
+       .executeUpdate()
     }
   }
+
+  /*
+  Implicit to change Blah into Json
+  Usage: Json.toJson(blah) -> returns JsValue
+         Json.toJson(blah).toString -> returns the Json string
+  */
+  implicit val blahWrites = new Writes[Blah] {
+    def writes(blah: Blah) = Json.obj(
+      "key" -> blah.key,
+      "value" -> blah.value
+    )
+  }
+
+  /*
+  Implicit to change Json to Blah
+  Usage:  blahjson.as[Blah] -> uses the implicit reader without validation
+          blahjson.asOpt[Blah] -> cast to option Blah without validation
+          blahjson.validate[Blah].get -> cast to Blah with validation
+          (blahjson \ "key").as[String] -> cast into string without validation
+          (blahjson \ "key").asOpt[String] -> cast into option string without validation
+          (blahjson \ "key").validate[String] -> validates the key is a string
+          (blahjson \ "key").validate[String].getOrElse("default_key") -> get the validated key
+  */
+  implicit val blahReads: Reads[Blah] = (
+    (JsPath \ "key").read[String] and
+    (JsPath \ "value").read[String]
+  )(Blah.apply _)
 
 }
 
@@ -41,6 +87,9 @@ object Blah {
 
 class Database extends Controller {
 
+  /*
+  Constructor, create table & insert some values
+  */
   DB.withConnection { implicit conn =>
     val createTable = SQL(
       """
@@ -49,7 +98,6 @@ class Database extends Controller {
       """
     ).execute()
   }
-
   Blah.create(Blah("key1","val1"))
   Blah.create(Blah("key2","val2"))
   Blah.create(Blah("key3","val3"))
@@ -58,7 +106,9 @@ class Database extends Controller {
   Blah.create(Blah("key6","val6"))
   Blah.create(Blah("key7","val7"))
 
-
+  /*
+  Check: Jdbc functionality
+  */
   def index = Action {
     var outstring = ""
     val conn = DB.getConnection()
@@ -74,7 +124,9 @@ class Database extends Controller {
     Ok(outstring)
   }
 
-
+  /*
+  Check: Jdbc query method
+  */
   def indexJdbc = Action {
     DB.withConnection { conn =>
       val stmt = conn.createStatement
@@ -87,16 +139,33 @@ class Database extends Controller {
         def next() = "Row:" + rs.getString(1)
       }.toStream
       Ok(outstream.mkString("\n"))
-    } 
+    }
   }
 
-
+  /*
+  Using Anorm
+  */
   def indexAnorm = Action {
     val allBlah = Blah.findAll()
-    Ok(allBlah.mkString("\n"))
+    val allBlahJson = Json.toJson(allBlah)
+    Ok(
+        "# Anorm SELECT * result as Blah case class:\n"
+      + allBlah.mkString("\n") + "\n\n"
+
+      + "# List head as Json:\n"
+      + Json.toJson(allBlah.head) + "\n\n"
+
+      + "# SELECT * result as Json:\n"
+      + Json.prettyPrint(allBlahJson) + "\n\n"
+
+      + "# From Json back into Seq[Blah]:\n"
+      + allBlahJson.asOpt[Seq[Blah]]
+    )
   }
 
-
+  /*
+  Not needed anymore, can use Blah.create() instead
+  */
   def insert(key:String, value:String) = {
     DB.withConnection { implicit conn =>
       val res = SQL(
@@ -107,10 +176,12 @@ class Database extends Controller {
     }
   }
 
-
+  /*
+  Check SQL feature with recursive CTE
+  */
   def sudoku = Action {
     val querystr = """
-    WITH 
+    WITH
     input(sud) AS (
        VALUES('53..7....6..195....98....6.8...6...34..8.3..17...2...6.6....28....419..5....8..79') --Medium
     )
@@ -124,7 +195,7 @@ class Database extends Controller {
         def next() = rs.getString(1)
       }.toStream
       Ok(outstream.mkString)
-    } 
+    }
   }
 
 }
